56a7b86b9ad59cffcdbe54b73eaaed1dd96ad773
david-sanvicente <sanvicented1@gmail.com>
2020-09-01 22:02:53 -0400

    Updates

---
+++ precache-manifest.5f5fd87f00d631d8096521ba5d928913.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "64f664fa8e7d682e2606",
+    "url": "/personal-portfolio/static/css/main.ebc1727c.chunk.css"
+  },
+  {
+    "revision": "64f664fa8e7d682e2606",
+    "url": "/personal-portfolio/static/js/main.7e73a216.chunk.js"
+  },
+  {
+    "revision": "199df5cd20e1a679bb0f",
+    "url": "/personal-portfolio/static/js/runtime~main.8fd806ec.js"
+  },
+  {
+    "revision": "81578a2f9324e5885477",
+    "url": "/personal-portfolio/static/js/2.1b180fc9.chunk.js"
+  },
+  {
+    "revision": "16809aa7430829676bea374431cd5308",
+    "url": "/personal-portfolio/static/media/chai.16809aa7.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/personal-portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "43d302c666a0bd3b6d24f4824a639d6f",
+    "url": "/personal-portfolio/static/media/git.43d302c6.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/personal-portfolio/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "a22a42410e2ce5842380688bac43bd03",
+    "url": "/personal-portfolio/static/media/nodejs.a22a4241.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/personal-portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "f2948be7bf211b1b2828c3aef8720a96",
+    "url": "/personal-portfolio/static/media/github-2.f2948be7.svg"
+  },
+  {
+    "revision": "32388bba7693ddbd664c04253a756c00",
+    "url": "/personal-portfolio/static/media/redux.32388bba.svg"
+  },
+  {
+    "revision": "0fb30b49da79da6979871081a23bfd53",
+    "url": "/personal-portfolio/static/media/express.0fb30b49.svg"
+  },
+  {
+    "revision": "43c9d125de48e59cb29a88dd1b20bc46",
+    "url": "/personal-portfolio/static/media/sequelize.43c9d125.svg"
+  },
+  {
+    "revision": "dc8c590334565b163ce4ac438de917af",
+    "url": "/personal-portfolio/static/media/sprayrbg.dc8c5903.jpg"
+  },
+  {
+    "revision": "e0bf717f6292d61fa1af3ee97b793f71",
+    "url": "/personal-portfolio/static/media/bg_old.e0bf717f.jpg"
+  },
+  {
+    "revision": "577d3c8d8588c91aa454e2ff4acdfd91",
+    "url": "/personal-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/david-sanvicente.github.io/precache-manifest.0388f2b04de5a5290d1d2b9a632cb9ad.js"
+  "/personal-portfolio/precache-manifest.5f5fd87f00d631d8096521ba5d928913.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/david-sanvicente.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/personal-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
