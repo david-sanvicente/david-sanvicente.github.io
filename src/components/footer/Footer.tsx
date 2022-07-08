c7bf1ffa6e7016a991f5481c29118e1d75af4d11
david-sanvicente <sanvicented1@gmail.com>
2022-07-08 15:17:13 -0400

    update email address in footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,25 +1,31 @@
-import React, {Component} from 'react';
-import ContactButtons from '../contactButtons/ContactButtons';
-import './styles.scss';
+import React, { Component } from "react";
+import ContactButtons from "../contactButtons/ContactButtons";
+import "./styles.scss";
 
 export default class Footer extends Component {
-    render(){
-        return (
-            <React.Fragment>
-                    <footer id="footer" className={`footer`}>
-                        <div className={`container`}>
-                            <h1>Contact Me
-                                <br /><small>Please reach out with any questions or feedback</small>
-                            </h1>
-                            <div id={`footer_buttons`}><ContactButtons /></div>                           
-                            <div id="emailMe">
-                                <a href={`mailto:david.sanvicente.js@gmail.com?subject = Feedback&body = Message`}>
-                                    <h3 id="emailMe">david.sanvicente.js@gmail.com</h3>
-                                </a>
-                            </div>
-                        </div>
-                    </footer>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <footer id="footer" className={`footer`}>
+          <div className={`container`}>
+            <h1>
+              Contact Me
+              <br />
+              <small>Please reach out with any questions or feedback</small>
+            </h1>
+            <div id={`footer_buttons`}>
+              <ContactButtons />
+            </div>
+            <div id="emailMe">
+              <a
+                href={`mailto:sanvicented1@gmail.com?subject = Feedback&body = Message`}
+              >
+                <h3 id="emailMe">sanvicented1@gmail.com</h3>
+              </a>
+            </div>
+          </div>
+        </footer>
+      </React.Fragment>
+    );
+  }
 }
